054aa7c74ee3fd6483a69ac6b58a4a459d5392c4
davin.bao <davin.bao@tpv-tech.com>
2025-02-20 17:06:01 +0800

    migrations/

--- server/database/migrations/0000_clammy_lila_cheney.sql
+++ server/database/migrations/0000_clammy_lila_cheney.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS "subscribe_user" (
 	"id" varchar PRIMARY KEY NOT NULL,
 	"email" varchar NOT NULL,
-	"name" varchar NOT NULL,
+	"name" varchar,
 	CONSTRAINT "subscribe_user_email_unique" UNIQUE("email")
 );
