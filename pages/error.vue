e08bba438286e1125fdc333b5528e07108ad9dae
davin.bao <davin.bao@tpv-tech.com>
2025-02-25 17:34:28 +0800

    Add error page

--- pages/error.vue
+++ pages/error.vue
@@ -1,14 +1,17 @@
 <script setup>
+
 definePageMeta({
   layout: false,
 })
+
+const props = defineProps({ error })
 </script>
 <template>
   <main class="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
     <div class="text-center">
-        <p class="text-base font-semibold text-accent">500</p>
-        <h1 class="mt-4 text-3xl font-bold tracking-tight text-primary-light sm:text-5xl">{{ $t("error.500.title") }}</h1>
-        <p class="mt-6 text-base leading-7 text-primary-light">{{ $t("error.500.tips") }}</p>
+        <p class="text-base font-semibold text-accent">{{ error.statusCode }}</p>
+        <h1 class="mt-4 text-3xl font-bold tracking-tight text-primary-light sm:text-5xl">{{ $t(`error.${error.statusCode}.title`) }}</h1>
+        <p class="mt-6 text-base leading-7 text-primary-light">{{ $t(`error.${error.statusCode}.tips`) }}</p>
         <div class="mt-10 flex items-center justify-center gap-x-6">
         <a href="/" class="rounded-md bg-accent px-3.5 py-2.5 text-sm font-semibold text-default shadow-sm hover:bg-accent-light focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">{{$t('toHomeBtn')}}</a>
         </div>
